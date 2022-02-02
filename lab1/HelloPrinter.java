ad8e4ce6de20d84515238a70c8bcd1817d9e534b
ikromshi <numanovikrom454@gmail.com>
2022-02-02 16:20:30 -0500

    starting two projects with java

---
+++ lab1/HelloPrinter.java
@@ -0,0 +1,9 @@
+package Java.lab1;
+
+public class HelloPrinter {
+    public static void main(String[] args)
+    {
+        double intToDouble = 2;
+        System.out.println(intToDouble);
+    }
+}
---
+++ lab1/lab01.java
@@ -0,0 +1,7 @@
+package Java.lab1;
+
+public class lab01 {
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+    }
+}
